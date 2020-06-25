4fb839e0b60fb53c88f672001e66f691a1b3c5a6
mohamedmuneeb1 <44756308+mohamedmuneeb1@users.noreply.github.com>
2020-06-25 12:07:11 +0300

    Add files via upload
    Template to implement back-end handling for switching queue functionality.

---
+++ SwitchQ.php
@@ -0,0 +1,16 @@
+<?php 
+
+require 'config/config.php';
+$id = $_POST["pid"];
+$name = $_POST["name"];
+$lname = $_POST["lname"];
+
+
+if($con){
+    //echo "connected....!";
+    //Implementation here.
+    echo "Switched successfully!" ;
+    
+    }
+
+?>
